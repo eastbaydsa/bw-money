0399c00e9171d93fb9b2aacab2137c19a4ebfc2e
Justin Allen <j@justinallen.us>
2018-08-26 13:04:12 -0700

    Basic page styles and elements (#1)
    * imports to app.scss
    
    * added svg assets from sketch
    
    * scaffolding basic page sections / styles
    
    * added new component for arrow divider
    
    * arrows placed, margins and padding tweaked

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from './components/header/header.component'
 import H2 from './components/h2/h2.component'
+import ARROW from './components/arrow/arrow.component'
 import './app.css'
 
 class App extends Component {
@@ -9,22 +10,55 @@ class App extends Component {
       <div>
         <Header />
         <H2>Their Money at Work</H2>
-        <div className="content">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et
-            massa enim. Proin condimentum sollicitudin felis a dapibus.
-            Pellentesque massa purus, venenatis sit amet euismod sed, accumsan
-            sed turpis.
-          </p>
-          <p>
-            Quisque sapien purus, semper aliquet ultrices eu, aliquet id felis.
-            Vivamus justo massa, elementum ac eleifend et, iaculis feugiat
-            neque. Etiam mollis dignissim risus, ac eleifend felis faucibus nec.
-            Suspendisse egestas dui in justo malesuada, eu varius urna
-            ultricies. Cras at aliquet felis. Nunc a euismod diam.
-          </p>
+        <div className="content panel dark" id="money">
+          <div className="inner">
+            <p>Illustration: donors give money (TK)</p>
+          </div>
         </div>
-      </div>
+        <ARROW />
+        <div className="content panel dark" id="politics">
+          <div className="inner">
+            <p>Illustration: politicians work on behalf of donors (TK)</p>
+          </div>
+        </div>
+        <ARROW />
+        <div className="content panel dark" id="workingclass">
+          <div className="inner">
+            <p>Illustration: working class gets the shaft (TK)</p>
+          </div>
+        </div>
+        <ARROW />                     
+        <H2>FAQ</H2>
+        <div className="content panel light">
+          <div className="inner">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et
+              massa enim. Proin condimentum sollicitudin felis a dapibus.
+              Pellentesque massa purus, venenatis sit amet euismod sed, accumsan
+              sed turpis.
+            </p>
+            <p>
+              Quisque sapien purus, semper aliquet ultrices eu, aliquet id felis.
+              Vivamus justo massa, elementum ac eleifend et, iaculis feugiat
+              neque. Etiam mollis dignissim risus, ac eleifend felis faucibus nec.
+              Suspendisse egestas dui in justo malesuada, eu varius urna
+              ultricies. Cras at aliquet felis. Nunc a euismod diam.
+            </p>
+          </div>
+        </div>
+        <H2>Fed Up?</H2>
+        <div className="content panel light">
+          <div className="inner">
+            <p>
+              Like it or not, we're all in a class struggle. Donors like these are setting the terms, but we have the strength, and it's not too late to fight back. 
+            </p>
+            <button className="blue space">DO SOMETHING</button>
+          </div>
+        </div>
+        <div className="content panel dark" id="footer">
+          <p>Created by East Bay DSA</p>
+        </div>
+    </div>      
     )
   }
 }
