51f363d62d2c0d43051c7d9debde49a6317a05bc
Sean Durham <sean@sdurham.net>
2018-08-28 20:00:15 -0700

    Remove menu

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,45 +1,15 @@
 import React, { Component } from 'react'
-import cn from 'classnames'
 
 import logo from '../../images/logo.png'
 import './header.css'
-import MenuToggle from '../menu/toggle.component.js'
-import Menu from '../menu/menu.component'
 
 class Header extends Component {
-  state = {
-    menuOpen: false
-  }
-
-  handleClose = () => {
-    this.setState({ menuOpen: false })
-  }
-
-  handleOpen = () => {
-    this.setState({ menuOpen: true })
-  }
-
   render() {
-    const { sectionLinks, socialLinks } = this.props
-    const { menuOpen } = this.state
-
     return (
       <div className="header__wrapper">
-        <div className={cn('header', { 'header--menu-open': menuOpen })}>
-          <div className="menu__toggle-wrapper">
-            <MenuToggle
-              open={menuOpen}
-              onClose={this.handleClose}
-              onOpen={this.handleOpen}
-            />
-          </div>
+        <div className="header">
           <img src={logo} alt="Buffy Wicks Money" />
         </div>
-        <Menu
-          open={menuOpen}
-          sectionLinks={sectionLinks}
-          socialLinks={socialLinks}
-        />
       </div>
     )
   }
