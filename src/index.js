ec7e08e591c8bd8049dd3b6d891e0693c7aa4ada
Max Lapides <max@maxlapides.com>
2018-09-10 20:52:28 -0700

    remove react-router google analytics

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import ReactRouterGoogleAnalytics from 'react-router-ga'
 
 import App from './app'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
-ReactDOM.render(
-  <Router>
-    <ReactRouterGoogleAnalytics id={process.env.REACT_APP_GOOGLE_ANALYTICS_ID}>
-      <App />
-    </ReactRouterGoogleAnalytics>
-  </Router>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
