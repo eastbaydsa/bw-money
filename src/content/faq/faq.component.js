be325963df7599d49268bf90787d1adef8be9dbc
Max Lapides <max@maxlapides.com>
2018-09-10 21:32:31 -0700

    add FAQ text

--- src/content/faq/faq.component.js
+++ src/content/faq/faq.component.js
@@ -7,18 +7,166 @@ export default function(props) {
       <H2>FAQ</H2>
       <div className="content panel light">
         <div className="inner">
+          <h3>What do you have against Buffy and her donors?</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et
-            massa enim. Proin condimentum sollicitudin felis a dapibus.
-            Pellentesque massa purus, venenatis sit amet euismod sed, accumsan
-            sed turpis.
+            Our politicians should work for us, represent us, and be accountable
+            to us – the ordinary working people of AD15. We don’t think someone
+            can take ten times the salary of a state representative from the one
+            percent and expect us to believe that she’s on our side.
           </p>
           <p>
-            Quisque sapien purus, semper aliquet ultrices eu, aliquet id felis.
-            Vivamus justo massa, elementum ac eleifend et, iaculis feugiat
-            neque. Etiam mollis dignissim risus, ac eleifend felis faucibus nec.
-            Suspendisse egestas dui in justo malesuada, eu varius urna
-            ultricies. Cras at aliquet felis. Nunc a euismod diam.
+            As socialists, we believe society should be run democratically and
+            for the benefit of people, not profit. Ordinary people should have
+            control over the things that matter. No one should face oppression,
+            state violence, or go without basic necessities like housing,
+            healthcare, or education.
+          </p>
+          <p>
+            But the superwealthy don’t agree. They are engaged in a long
+            running, well funded, and ruthless campaign to take ever more for
+            themselves and leave crumbs to the rest of us. They don’t want to
+            pay taxes, pay the workers that made them rich, or pay for the
+            damageharms their profit-obsessed companies do to the world.
+          </p>
+          <p>To wage war on working people, the wealthy coordinate to:</p>
+          <ul>
+            <li>
+              Buy elections at every level of the government through unlimited
+              donations to dark money PACs,
+            </li>
+            <li>
+              Influence policy and media by funding think tanks and foundations
+              to advance their agenda,
+            </li>
+            <li>
+              And fight tooth and nail against anything that would transfer
+              resources, power, and security to working people, like labor
+              protections, the expansion of the social safety net, and big
+              reforms like Medicare for All, the repeal of Costa Hawkins, and
+              universal free college tuition.
+            </li>
+          </ul>
+          <p>
+            We don’t think most of these donors care much about Buffy. They pool
+            their money and offer it to thousands of candidates like her across
+            the country, at every level of government. This is how they block
+            legislation that favors working people, while passing policies that
+            favor the rich, like tax cuts, corporate giveaways, and harmful
+            deregulation.
+          </p>
+          <p>
+            We have the resources to fix the many serious problems we face, but
+            we won’t fix them by continuing to elect politicians who put on a
+            progressive face while raking in dark money and protecting the
+            status quo at all costs.
+          </p>
+          <h3>
+            I definitely don’t want to vote for Buffy. Who should I vote for
+            instead?
+          </h3>
+          <p>
+            <a href="https://www.jovanka.org/">Jovanka Beckles</a> is a
+            corporate-free candidate and a democratic socialist with a long
+            history of fighting and winning for working people in the East Bay.
+            She’s served on the Richmond City Council and is a leader in the
+            Richmond Progressive Alliance (RPA). On the Richmond City Council,
+            Jovanka and her colleagues from the RPA took on Chevron and its
+            millions and won victories like a $15 minimum wage, citywide rent
+            control, and banning the box.
+          </p>
+          <p>
+            <a href="https://www.jovanka.org/platform">Jovanka’s platform</a> is
+            a bold vision for a California that works for the many, not the few.
+            She knows that in a state with a $2.7 trillion GDP, we already have
+            already created more than enough wealth to provide everyone with a
+            stable, safe, and dignified life. Most importantly, Jovanka
+            understands how power works, and that it
+          </p>
+          <p>
+            She’ll fight for a single-payer Medicare for All system that covers
+            healthcare for everyone in California. She’ll work to end Costa
+            Hawkins and implement statewide rent control. Jovanka has a plan for
+            free universal pre-K and a moratorium on charter schools. She
+            supports labor with a $20 minimum wage and stronger protections for
+            workers.
+          </p>
+          <p>
+            Because Jovanka doesn’t have Buffy’s millions, she depends on
+            ordinary people like you and me to talk to our neighbors and friends
+            about sending her to Sacramento. Come to one of East Bay DSA’s{' '}
+            <a href="http://www.eastbaydsa.org/events">
+              canvasses or phonebanks
+            </a>{' '}
+            between now and November! No experience necessary – we’ll train you
+            and you’ll have a good time!
+          </p>
+          <p>
+            (Jovanka and her campaign were not involved in the creation of this
+            website.)
+          </p>
+          <h3>
+            Who made this site? How can I learn more about DSA and get involved
+            in the election?
+          </h3>
+          <p>
+            We are your friendly local{' '}
+            <a href="http://www.eastbaydsa.org">East Bay chapter</a> of the{' '}
+            <a href="http://www.dsausa.org/">
+              Democratic Socialists of America
+            </a>
+            ! We are solely responsible for this site. We want a world run for
+            the benefit of people, not profit. We believe in real democracy,
+            which means ordinary working people having democratic control over
+            the things that matter. We pick big fights that will benefit working
+            people, like{' '}
+            <a href="https://medicareforall.dsausa.org/">Medicare for All</a>{' '}
+            and the{' '}
+            <a href="https://voteyesonprop10.org/">repeal of Costa-Hawkins</a>;
+            we support strong, democratic labor unions; and we back democratic
+            socialist political candidates like{' '}
+            <a href="https://www.jovanka.org/">Jovanka Beckles</a>, who is
+            running against Buffy. And we have fun doing it! Want to check us
+            out? Come out to one of our{' '}
+            <a href="http://www.eastbaydsa.org/resources/readings/socialist-school/2018/summer">
+              Socialist Night Schools
+            </a>{' '}
+            or an{' '}
+            <a href="http://www.eastbaydsa.org/events">
+              Intro to Democratic Socialism event
+            </a>
+            ! Follow us on{' '}
+            <a href="https://www.facebook.com/eastbaydsa">Facebook</a> or{' '}
+            <a href="https://twitter.com/DSAEastBay">Twitter</a>! Sign up for
+            our <a href="http://www.eastbaydsa.org/join">newsletter</a> to learn
+            about events and actions! What Assembly District am I in?{' '}
+            <a href="http://findyourrep.legislature.ca.gov/">Click here</a> to
+            find out! Assembly District 15 covers parts Alameda (Albany,
+            Berkeley, Emeryville, Piedmont, parts of Oakland) and Contra Costa
+            (El Cerrito, Hercules, Pinole, Richmond, San Pablo, El Sobrante, and
+            Kensington) counties. When is the election, where is my polling
+            place, and am I registered to vote? The general election is Nov. 6,
+            2018.{' '}
+            <a href="http://www.sos.ca.gov/elections/polling-place/">
+              Find your polling place here
+            </a>
+            . <a href="https://registertovote.ca.gov/">Register to vote here</a>{' '}
+            before Oct. 15. After Oct. 15,{' '}
+            <a href="http://www.sos.ca.gov/elections/voter-registration/conditional-voter-reg/">
+              here’s how you can vote conditionally
+            </a>
+            .
+          </p>
+          <h3>
+            I have a corporate candidate running in my local election. How can I
+            make a site like this?
+          </h3>
+          <p>
+            Hit us up at{' '}
+            <a href="mailto:info@eastbaydsa.org">info@eastbaydsa.org</a>! We can
+            share the code for this site and tell you how we did the research.
+            It’s fun! How can I get in touch with you? Find us at{' '}
+            <a href="mailto:info@eastbaydsa.org">info@eastbaydsa.org</a>. Say
+            hi!
           </p>
         </div>
       </div>
