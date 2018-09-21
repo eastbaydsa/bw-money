b156200e1e1f09887fcbf6f51f72b688c61bdcf4
Max Lapides <max@maxlapides.com>
2018-09-21 00:08:44 -0700

    add twitter link to header

--- src/components/social-links/link.component.js
+++ src/components/social-links/link.component.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 export default function SocialLink({ label, url, icon }) {
   return (
-    <a className="social-links__link" href={url}>
+    <a
+      className="social-links__link"
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img width="30" height="30" src={icon} alt={label} />
     </a>
   )
--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -1,7 +1,11 @@
-import facebookIcon from '../images/facebook.svg'
+// import facebookIcon from '../images/facebook.svg'
 import twitterIcon from '../images/twitter.svg'
 
 export default [
-  { label: 'Twitter', url: 'https://twitter.com', icon: twitterIcon },
-  { label: 'Facebook', url: 'https://facebook.com', icon: facebookIcon }
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/buffydotmoney',
+    icon: twitterIcon
+  }
+  // { label: 'Facebook', url: 'https://facebook.com', icon: facebookIcon }
 ]
