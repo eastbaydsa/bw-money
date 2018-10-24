624ae878580e12fc8aa23a0de723fc9367984f38
Max Lapides <max@maxlapides.com>
2018-10-24 16:39:09 -0700

    update donation total

--- src/components/intro/intro.component.js
+++ src/components/intro/intro.component.js
@@ -15,7 +15,7 @@ const Intro = () => (
         prefix="$"
         delay={0.5}
         start={0}
-        end={2111605}
+        end={2472164}
         separator=","
         duration={2.5}
         useEasing
